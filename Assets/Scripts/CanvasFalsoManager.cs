0ba3a4974095a3f3793ff514690a67b737d389c4
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-10-16 19:47:08 +0200

    Escena 0

---
+++ Assets/Scripts/CanvasFalsoManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasFalsoManager : MonoBehaviour
+{
+    public void EmpezarPartida()
+    {
+        //Cargar escena 1
+        SceneManager.LoadScene(1);
+
+    }
+    public void TerminarJuego()
+    {
+        //Solo funciona en el ejecutable
+        Application.Quit();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
