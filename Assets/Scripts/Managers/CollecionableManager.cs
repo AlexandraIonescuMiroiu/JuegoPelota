95ac32d516dae3a9084511dc4cd46e561b15ee53
Lyannis <lyannisart@gmail.com>
2024-11-09 01:53:32 +0100

    terrible

--- Assets/Scripts/Managers/CollecionableManager.cs
+++ Assets/Scripts/Managers/CollecionableManager.cs
@@ -6,16 +6,27 @@ using TMPro;
 public class CollecionableManager : MonoBehaviour
 {
     private int collecionableCounter = 0;
+    [SerializeField] AudioSource sfx;
+    [SerializeField] AudioClip clip;
     [SerializeField] private TMP_Text CollecionableCounter;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coleccionable"))
         {
+            ReproducirSonido(clip);
             Destroy(other.gameObject);
             collecionableCounter++;
             CollecionableCounter.text = collecionableCounter.ToString();
             Debug.Log(collecionableCounter);
         }
+    }
+
+    private void ReproducirSonido(AudioClip clip)
+    {
+        sfx.PlayOneShot(clip);
+
+
+
     }
 }
